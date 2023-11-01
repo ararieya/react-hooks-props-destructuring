015de5fd428bf558f5a3a10524de2dcb1fb3a6fd
Ashley Rarieya <ararieya@gmail.com>
2023-11-01 10:49:22 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,51 @@ function App() {
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  const socialLinks = {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      {/* passing down props from the parent component */}
+      <MovieCard title={title} posterSrc={posterURL} genres={genresArr} />
+      <SocialMedia links={socialLinks} />
+    </div>
+  );
+  
+}
+
+// child component- Without Destructing
+function MovieCard(props) {
+  return (
+    <div className="movie-card">
+      <img src={props.posterSrc} alt={props.title} />
+      <h2>{props.title}</h2>
+      <small>{props.genres.join(", ")}</small>
     </div>
   );
 }
+
+//With Destructing
+// function MovieCard({ title, posterSrc, genres }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
+
+function SocialMedia({ socialLinks }) {
+  return (
+    <div>
+      <a href={socialLinks.github}>{socialLinks.github}</a>
+      <a href={socialLinks.linkedin}>{socialLinks.linkedin}</a>
+    </div>
+  );
+}
+
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
 }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
-      <h2>{title}</h2>
-      <small>{genres.join(", ")}</small>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM"
+        alt="Mad Max: Fury Road"
+      />
+      <h2>Mad Max: Fury Road</h2>
+      <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
     </div>
   );
 }
